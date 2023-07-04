c31612c1b95d3cd34736c9b4b8f8cc20443f5809
Atomico592 <atomico592@gmail.com>
2023-07-04 18:33:47 +0600

    Refactoring example logic

--- src/components/Example/Exampl.js
+++ src/components/Example/Exampl.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Cell from "../UI/Cell/Cell";
 import "./Examlp.css"
+import Info from "../UI/Info/Info";
 
 const Exampl = () => {
-    const contr = [0, 9, 19, 29, 30]
+    const [state, setState] = useState(null);
+    const [openPopup, setOpenPopup] = useState(false);
+    const contr = [0, 9, 19, 29, 30];
+
+
+    const onClick = (item, e) => {
+        setState({contr : item, posY : e.clientY, posX: e.clientY});
+        if (state) {
+            setOpenPopup(true);
+        }
+
+    }
+
+    if (openPopup) {
+        setTimeout(() => {
+            setOpenPopup(false);
+        }, 5000);
+
+    }
+
+
     return (
+        <div>
         <div className="exampl">
             <p className="text"> Меньше</p>
             {contr.map(item => (
                 <Cell
                     key={item + 1}
                     color={item}
+                    onClick={(e) => onClick(item, e)}
                 />
             ))}
             <p className="text">
                 Больше
             </p>
+        </div>
+            {openPopup ? ( <Info
+                contributions={state?.contr}
+                position={{left: `${state?.positionX}px`, top: `${state?.positionY}px`}}
+            />) : null}
+
         </div>
     );
 };
